787b9abf8ec49bb9d9254753ed8eb58abcb4d94d
Hasnat <webdevhasnat@gmail.com>
2023-02-18 09:04:08 +0600

    hero section done

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      xsm: "380px",
+      sm: "550px",
+
+      md: "720px",
+
+      lg: "900px",
+
+      xl: "1280px",
+
+      "2xl": "1536px",
+    },
+  },
+  extend: {
+    colors: {
+      headingClr: "#222",
+      textGrayClr: "#565656",
+      whiteBg: "#ffffff",
+      primaryBg: "#f6f9fe",
+      primaryClr: "#7c4fe0",
+    },
+  },
+
+  plugins: [],
+};
